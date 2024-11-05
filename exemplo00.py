b31b9c484d1394db5c563b0e1a6e2a57cdcf911d
BraulioHermanson <brauliohcampos0@outlook.com>
2024-11-05 14:28:47 -0300

    commit

--- exemplo00.py
+++ exemplo00.py
@@ -1,10 +1,25 @@
 import requests
+from pydantic import BaseModel
 
-# requests.get #select
+class PokemonSchame(BaseModel): # contrato de dados, schema de dados, a view da API
+    name: str
+    type: str
 
-# requests.post #create
+    class Config:
+        from_attributes = True
 
-# requests.put #update
+def pegar_pokemon(id: int) -> PokemonSchame:
+    # requests.get #select
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{id}")
+    data = response.json()
+    data_types = data['types']
+    types_list = []
+    for type_info in data_types:
+        types_list.append(type_info['type']['name'])
+    types = ', '.join(types_list)
+    return PokemonSchame(name =data['name'],type = types)
 
-# requests.delete #delete
+if __name__ == "__main__":
+    print(pegar_pokemon(373))
+    print(pegar_pokemon(376))
 
